617a3695a770cc1cbc987567498f186f30560653
“Angel <angel.pena5426@alumnos.udg.mx>
2026-05-18 23:51:33 -0600

    feat: add custom cuceiverse app logo

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -103,7 +103,7 @@ export const MainLayout: React.FC = () => {
       <nav className="navbar glass-panel flex-none">
         <div className="nav-brand">
           <div className="nav-logo" aria-label="CUCEI">
-            <img src="/logo-cucei.svg" alt="CUCEI" />
+            <img src="/cuceiverse-logo.svg" alt="CuceiVerse" />
           </div>
           <h2>CUCEIVERSE</h2>
         </div>
